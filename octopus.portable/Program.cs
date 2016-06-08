90ae2efdd0922275c28995554374b1c6de3aded6
Дмитрий Гринь <dg@jooble.com>
2016-06-08 14:50:44 +0300

    octopus.workers, octopus.portable created

---
+++ octopus.portable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace octopus.portable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
---
+++ octopus.workers/Common/Settings.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace octopus.workers.Common
+{
+	/// <summary>
+	/// Worker settings collection
+	/// </summary>
+	class Settings
+	{
+		/// <summary>
+		/// Settings storage
+		/// </summary>
+		private Dictionary<string, string> _settings;
+
+		/// <summary>
+		/// ctor
+		/// </summary>
+		public Settings()
+		{
+			_settings = new Dictionary<string, string>();
+		}
+
+		/// <summary>
+		/// Sets setting value
+		/// </summary>
+		public void SetValue(string key, string value)
+		{
+			if (!_settings.ContainsKey(key))
+				_settings.Add(key, value);
+			else
+				_settings[key] = value;
+		}
+
+		/// <summary>
+		/// Gets setting value or empty string if settings does not exists
+		/// </summary>
+		public string GetValue(string key)
+		{
+			if (_settings.ContainsKey(key))
+				return _settings[key];
+			else
+				return string.Empty;
+		}
+	}
+}
